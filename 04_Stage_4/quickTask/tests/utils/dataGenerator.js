a350b4f3bd7d45f5c178ae54b67160548a832f50
tomilissa <tomi.lissarrague93@gmail.com>
2026-02-15 18:21:33 -0300

    Quick Taks Stage 4 - Update

--- 04_Stage_4/quickTask/tests/utils/dataGenerator.js
+++ 04_Stage_4/quickTask/tests/utils/dataGenerator.js
@@ -2,8 +2,3 @@ export function generateRandomEmail() {
   const timestamp = Date.now();
   return `qa_${timestamp}@mail.com`;
 }
-
-
-export function generateRandomLastName(prefix = 'QA') {
-  return `${prefix}_${Math.floor(Math.random() * 1000)}`;
-}
